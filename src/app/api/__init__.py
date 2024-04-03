b799e1775080c0f4e5436945333ca8e77dd6a4d8
Daniil Protasenia <danikprotaseney@gmail.com>
2024-04-03 17:48:48 +0300

    feat(__init__): add imports to __all__ for metrics and scale

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -1,6 +1,5 @@
 from .container.router import router as container
-
-# from .metrics.router import router as metrics
-# from .scale.router import router as scale
+from .metrics.router import router as metrics
+from .scale.router import router as scale
 
 __all__ = ["container", "metrics", "scale"]
