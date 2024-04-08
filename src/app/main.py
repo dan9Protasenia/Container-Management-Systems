17017602af806b59efc46f1e0899c363f5f3ae6e
Daniil Protasenia <danikprotaseney@gmail.com>
2024-04-08 10:12:52 +0300

    feat(main): add Instrumentator().instrument(app).expose(app) for metrics

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from prometheus_fastapi_instrumentator import Instrumentator
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path=dotenv_path)
@@ -10,6 +11,7 @@ from src.app.startup import create_app
 
 # config = MainConfig()
 app = create_app()
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     import uvicorn
