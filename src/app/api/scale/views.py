10649467db9b8a568293726a89feb525b079aadb
Daniil Protasenia <danikprotaseney@gmail.com>
2024-04-08 10:08:49 +0300

    feat(scale_views): add proxy_request

--- src/app/api/scale/views.py
+++ src/app/api/scale/views.py
@@ -1,10 +1,12 @@
 from fastapi import Body, Path
 
 from src.app.api.container.service import ContainerService
-from src.app.api.scale.service import ScaleService
+from src.app.api.scale.service import ScaleService, LoadBalancer
+from src.app.core.schemas.container import ScaleRequest
 
 container_service = ContainerService()
 scale_service = ScaleService()
+load_service = LoadBalancer()
 
 
 async def scale_container(container_id: str = Path(...), scale_target: int = Body(...)):
@@ -17,9 +19,11 @@ async def get_containers_count_by_image(image_name: str):
     return {"image_name": image_name, "containers_count": containers_count}
 
 
-async def load_balance():
-    target_container_id = scale_service.balance_load()
-    if not target_container_id:
-        return {"error": "Нет доступных контейнеров для балансировки"}
+async def scale(request: ScaleRequest):
+    target_container_id = scale_service.scale(request.container_id, request.scale_target)
 
     return {"message": f"Запрос отправлен на контейнер {target_container_id}"}
+
+
+async def proxy_request(path: str):
+    return await load_service.proxy_request(path)
